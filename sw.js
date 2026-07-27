10e55aa99f99a7482578b6c264019a9b6b367ffa
ADopke <abdopke@gmail.com>
2026-07-27 14:04:38 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // o mesmo número que está em index.html. É isso que faz o navegador descartar
 // o cache antigo e buscar os arquivos novos.
 
-const VERSAO = "1.7.1";
+const VERSAO = "1.7.2";
 const CACHE = "hbier-" + VERSAO;
 
 // o mínimo para a tela abrir offline
